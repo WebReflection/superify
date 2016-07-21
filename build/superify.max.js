adeec1ebf475b4269577ed7d8954268381c64ca6
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-07-21 21:50:19 +0100

    first commit

--- build/superify.max.js
+++ build/superify.max.js
@@ -20,4 +20,153 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-var main = {};
+var superify = (function (Object) {
+var
+  has = Object.hasOwnProperty,
+  defineProperty = Object.defineProperty,
+  getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor,
+  getOwnPropertyNames = Object.getOwnPropertyNames,
+  getPrototypeOf = Object.getPrototypeOf,
+  setPrototypeOf = Object.setPrototypeOf ||
+                   function (o, p) { o.__proto__ = p; },
+  wm = typeof WeakMap === 'function' ?
+    new WeakMap() :
+    (function (k, v) {
+      function reset() {
+        k = [];
+        v = [];
+      }
+      reset();
+      setInterval(reset, 1000);
+      return {
+        get: function (K) {
+          return v[k.indexOf(K)];
+        },
+        set: function (K, V) {
+          v[k.push(K) - 1] = V;
+        }
+      };
+    }()),
+  descriptor = {
+    configurable: true,
+    get: function get() {
+      return wm.get(this) || superable(this);
+    }
+  }
+;
+
+function crawl(self) {
+  var
+    Super = function () {
+      return constructor.apply(this, arguments);
+    },
+    constructor = trap(Super, 'constructor', 'value'),
+    proto = self,
+    i, desc, name, names, tobeDefined
+  ;
+  while ((proto = getPrototypeOf(proto))) {
+    names = getOwnPropertyNames(proto);
+    i = names.length;
+    while (i--) {
+      name = names[i];
+      if (!has.call(Super, name)) {
+        tobeDefined = false;
+        desc = getOwnPropertyDescriptor(proto, name);
+        if (has.call(desc, 'value')) {
+          if (typeof desc.value === 'function') {
+            tobeDefined = true;
+            desc.value = trap(Super, name, 'value');
+          }
+        } else {
+          if (has.call(desc, 'get')) {
+            tobeDefined = true;
+            desc.get = trap(Super, name, 'get');
+          }
+          if (has.call(desc, 'set')) {
+            tobeDefined = true;
+            desc.set = trap(Super, name, 'set');
+          }
+        }
+        if (tobeDefined) {
+          defineProperty(Super, name, desc);
+        }
+      }
+    }
+  }
+  wm.set(Super, self);
+  return Super;
+}
+
+function superable(self) {
+  var Super = crawl(self);
+  wm.set(self, Super);
+  return Super;
+}
+
+function trap(Super, property, key) {
+  return function () {
+    var
+      self = wm.get(Super),
+      proto = getPrototypeOf(self),
+      parent = self,
+      desc,
+      parentDesc
+    ;
+    do {
+      desc = getOwnPropertyDescriptor(parent, property);
+    } while (
+      !desc && (parent = getPrototypeOf(parent))
+    );
+    while ((parent = getPrototypeOf(parent))) {
+      parentDesc = getOwnPropertyDescriptor(parent, property);
+      if (parentDesc && parentDesc[key] !== desc[key]) {
+        setPrototypeOf(self, parent);
+        try {
+          result = parentDesc[key].apply(self, arguments);
+          setPrototypeOf(self, proto);
+        } catch(e) {
+          setPrototypeOf(self, proto);
+          throw e;
+        }
+        return result;
+      }
+    }
+  };
+}
+
+function superify(object) {
+  defineProperty(
+    typeof object === 'function' ?
+      object.prototype : object,
+    'super',
+    descriptor
+  );
+  return object;
+}
+
+/* faster way for methods only
+function trapMethod(Super, property) {
+  return function () {
+    var
+      self = wm.get(Super),
+      method = self[property],
+      proto = getPrototypeOf(self),
+      parent = proto,
+      result
+    ;
+    while (method === parent[property]) {
+      parent = getPrototypeOf(parent);
+    }
+    setPrototypeOf(self, parent);
+    try {
+      result = parent[property].apply(self, arguments);
+      setPrototypeOf(self, proto);
+    } catch(e) {
+      setPrototypeOf(self, proto);
+      throw e;
+    }
+    return result;
+  };
+}
+// */
+}(Object));
--- test/superify.js
+++ test/superify.js
@@ -1,13 +1,68 @@
 //remove:
-var main = require('../build/superify.node.js');
+var superify = require('../build/superify.node.js');
 //:remove
 
 wru.test([
   {
     name: "main",
     test: function () {
-      wru.assert(typeof main == "object");
-      // wru.assert(0);
+      wru.assert(typeof superify == "function");
+    }
+  }, {
+    name: 'basic',
+    test: function () {
+
+      var result = [];
+
+      function A() {
+        result.push('A');
+      }
+
+      A.prototype = superify({
+        constructor: A,
+        method: function () {
+          result.push('method from A');
+        },
+        get runtime() {
+          result.push('runtime A getter');
+        }
+      });
+
+      function B() {
+        this.super();
+        result.push('B');
+      }
+
+      B.prototype = Object.create(A.prototype, {
+        constructor: {value: B},
+        method: {value: function () {
+          this.super.method();
+          result.push('method from B');
+        }}
+      });
+
+      function C() {
+        this.super();
+        result.push('C');
+      }
+
+      C.prototype = Object.create(B.prototype, {
+        constructor: {value: C},
+        runtime: {get: function () {
+          this.super.runtime;
+          result.push('runtime C getter');
+        }}
+      });
+
+      var c = new C;
+      wru.assert(result.splice(0, result.length).join(',') === 'A,B,C');
+
+      c.method();
+      wru.assert(result.splice(0, result.length).join(',') === 'method from A,method from B');
+
+      c.runtime;
+      wru.assert(result.splice(0, result.length).join(',') === 'runtime A getter,runtime C getter');
+
     }
   }
 ]);
